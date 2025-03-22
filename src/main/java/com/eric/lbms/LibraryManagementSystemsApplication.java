d61286b4fb2a3a7366b0d740234c2aabab006204
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-03-22 09:51:25 +0200

    feature: Project Initialization

---
+++ src/main/java/com/eric/lbms/LibraryManagementSystemsApplication.java
@@ -0,0 +1,13 @@
+package com.eric.lbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemsApplication.class, args);
+	}
+
+}
